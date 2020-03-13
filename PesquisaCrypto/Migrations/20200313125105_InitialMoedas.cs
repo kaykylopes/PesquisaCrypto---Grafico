c5a0b16367edb177a5acbf2063dade79e0d09295
david.mrcs@hotmail.com <david.mrcs@hotmail.com>
2020-03-13 10:08:30 -0300

    Adicionar arquivos de projeto.

---
+++ PesquisaCrypto/Migrations/20200313125105_InitialMoedas.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PesquisaCrypto.Migrations
+{
+    public partial class InitialMoedas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
